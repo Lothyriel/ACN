d14440f30ea429e73532c4c56235d5c4661c3215
João Xavier <fastjonh@gmail.com>
2022-11-24 02:38:22 -0300

    fim

--- src/ACN.py
+++ src/ACN.py
@@ -44,7 +44,6 @@ class ACN(commands.Bot):
         self.debug = False
 
         load_dotenv()
-        print(f'token {os.getenv("TOKEN_BOT")}')
         self.run(os.getenv("TOKEN_BOT"))
 
     @commands.Cog.listener()
